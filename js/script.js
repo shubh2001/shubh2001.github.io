063a2091f5d361addf0889477974694b9bd6fcfc
Shubh Singhal <shubhsinghal02@gmail.com>
2020-07-25 17:54:17 +0530

    Captston Final

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ window.onscroll = function () {scrollfunction()};
 function scrollfunction () {
     if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
         $('nav').addClass('bg-dark')
-        $('.nav-link').addClass('jsover')
+        
         
     }
     else {
         $('nav').removeClass('bg-dark') 
-        $('.nav-link').removeClass('jsover')   }
+          }
 }
 
