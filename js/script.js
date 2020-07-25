4b55d671c00da3cdc9b18ad91beb507ab46c6913
Shubh Singhal <shubhsinghal02@gmail.com>
2020-07-25 18:10:42 +0530

    Capstone

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ window.onscroll = function () {scrollfunction()};
 
 function scrollfunction () {
     if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
-        $('nav').addClass('bg-dark')
+        $('nav').addClass('bg-dark');
         
         
     }
     else {
-        $('nav').removeClass('bg-dark') 
+        $('nav').removeClass('bg-dark');
           }
 }
 
